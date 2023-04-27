ce968307fa539deb5fb231854e272b9888debd84
lukass1278 <117936519+lukass1278@users.noreply.github.com>
2023-04-27 15:43:25 -0300

    Initial commit

---
+++ Login Caminhoneiro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Login_Caminhoneiro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
